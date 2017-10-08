62d85d691e5360c0b75ea03743d0b2362acf9836
flashman2000 <32598745+flashman2000@users.noreply.github.com>
2017-10-07 22:06:29 -0500

    v1.2.4

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Vu_action.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Vu_action.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 /**
- * Created by Flash on 10/7/2017.
+ * Created by Flash on 10/7/2017
  **/
 
 @Autonomous(name = "vutest")
